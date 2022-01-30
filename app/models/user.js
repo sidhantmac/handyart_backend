8b9a8b6180c0f029d502f7e15a8fd8e902ed3b8c
Sidhant <sidhant.madkaikar@gmail.com>
2022-01-30 12:38:32 +0530

    Saving user

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
     const user = sequelize.define("users", {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
       name: {
         type: Sequelize.STRING
       },
--- index.js
+++ index.js
@@ -28,3 +28,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
+// Refernces
+// https://www.bezkoder.com/node-js-sql-server-crud/
